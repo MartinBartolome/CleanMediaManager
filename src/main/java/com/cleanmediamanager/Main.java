5fec0c1c549459f677a2280b10715c3c01cb90d3
Martin Bartolomé <mail@martinbartolome.ch>
2026-04-23 15:56:47 +0200

    Fixed Icon

--- src/main/java/com/cleanmediamanager/Main.java
+++ src/main/java/com/cleanmediamanager/Main.java
@@ -5,6 +5,10 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
+        // Must be set before any AWT/Swing call so that the WM_CLASS of the
+        // window matches StartupWMClass in the .desktop file – required for
+        // GNOME/Ubuntu to display the correct taskbar icon.
+        System.setProperty("sun.awt.appName", "CleanMediaManager");
         SwingUtilities.invokeLater(MainWindow::new);
     }
 }
